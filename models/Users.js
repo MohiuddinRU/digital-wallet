2abf18f1730ac0749402087a1c0e5c9aac20a920
MohiuddinRU <mohiuddin.ice.ru@gmail.com>
2021-08-02 15:52:32 +0600

    modified User model file

--- models/Users.js
+++ models/Users.js
@@ -1,13 +1,78 @@
 /** @format */
 
-//dependencise
-const axios = require('axios');
+const variables = require("../variables");
+const mongoose = require("mongoose");
+const md5 = require("md5");
+const Schema = mongoose.Schema;
 
-//internal dependencise
-const { currencyConverter } = require('../variables/index');
+mongoose.connect("mongodb+srv://root:VrIN2J6BXmT6cw8q@cluster0.xczmd.mongodb.net/summer?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
 
-exports.convertCurrency = (from, to, amount) => {
-  return axios
-    .get(`${currencyConverter}/convert/${from}/${to}/${amount}`)
-    .then((response) => response.data);
+const ObjectId = Schema.ObjectId;
+
+const userSchema = new Schema({
+  fullName: String,
+  email: String,
+  phoneNumber: String,
+  password: String,
+  image: String
+});
+const UserModel = mongoose.model("Users", userSchema);
+
+exports.checkDuplicacy = (email) => {
+  return UserModel.findOne({ email });
 };
+exports.checkExistence = (email, password) => {
+  //console.log((1));
+
+  return UserModel.findOne({ email, password: md5(password) });
+};
+
+exports.create = (userInfo) => {
+  userInfo.password = md5(userInfo.password);
+  //console.log(userInfo);
+  return UserModel.create(userInfo);
+};
+
+exports.checkUser = (id) => {
+  //return { fullNme: "Shuhail alam" };
+  return UserModel.findOne({ _id: id });
+};
+
+//checkExistMail
+exports.checkMailId = (emailInfo) => {
+  return UserModel.findOne({ email: emailInfo });
+};
+
+//checkuserDetails for Reset password
+
+exports.checkDetails = (_id, email) => {
+  return UserModel.findOne({ _id, email });
+};
+
+//User password Updated
+
+exports.updatePassword = (userid, password) => {
+  return UserModel.findOneAndUpdate({ _id: userid, password });
+};
+exports.updateUser = (userId, newEmail) => {
+  return UserModel.findOneAndUpdate(
+    { _id: userId },
+    { email: newEmail },
+    { new: true }
+  );
+};
+
+exports.updateUserInfo = async(userId, userInfo) => {
+    return await UserModel.findOneAndUpdate({_id: userId}, userInfo);
+}
+
+//finding user details for order 
+exports.findUser = (userid)=>{
+  return UserModel.findOne({_id: userid});
+};
+
